6c2d027c9f8425d86f69eb857710e81d42369c2c
Jannis Leidel <jannis@leidel.info>
2009-04-07 22:07:24 +0200

    Changed package name to django-atomformat

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(
-    name='django-atompub',
+    name='django-atomformat',
     version='0.1.0dev',
     author='James Tauber',
     url='http://code.google.com/p/django-atompub/',
