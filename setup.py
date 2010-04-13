bf81bcdac30a2df056d27f955263c7ed572d6184
Brian Rosner <brosner@gmail.com>
2010-04-12 18:31:17 -0600

    PEP 386 compliant version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='django-atomformat',
-    version='0.1.1dev',
+    version='0.1.1.dev1',
     author='James Tauber',
     url='http://code.google.com/p/django-atompub/',
     py_modules=['atomformat'],
